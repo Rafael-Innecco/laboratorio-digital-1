32d734d9127be93a9aec40783e0f01a661e5e911
Rafael Innecco <rafael.a.innecco@usp.br>
2023-03-25 18:14:08 -0300

    Lógica de conversão para decimal

---
+++ Projeto/Semana_3/hexDecimal.vhd
@@ -0,0 +1,86 @@
+--------------------------------------------------------------------
+-- Arquivo   : hexDecimal.vhd
+-- Projeto   : Projeto PCS3635 2023
+--------------------------------------------------------------------
+-- Descricao :
+--    Unidade combinatória que converte um dígito hexadecimal para decimal, com carry out
+--------------------------------------------------------------------
+-- Revisoes  :
+--     Data        Versao  Autor             Descricao
+--     10/11/2021  1.0     Rafael Innecco    Versão inicial
+--------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity hexDecimal is
+    port (
+        hexa    : in std_logic_vector (3 downto 0);
+        Cin     : in std_logic;
+        dec     : out std_logic_vector (3 downto 0);
+        Cout    : out std_logic
+    );
+end entity hexDecimal;
+
+architecture dataflow of hexDecimal is
+    component somador is
+        generic (
+            size: natural := 8
+        );
+        port (
+            A, B    : in std_logic_vector (size - 1 downto 0);
+            S       : in std_logic;
+            F       : out std_logic_vector (size-1 downto 0);
+            Z       : out std_logic;
+            N       : out std_logic;
+            Ov      : out std_logic;
+            Co      : out std_logic
+        );
+    end component;
+
+    signal hexa_ext, hexa_int, dec_ext, Cin_ext: std_logic_vector (4 downto 0);
+    signal dec_int, hexa_2 : std_logic_vector (4 downto 0);
+    signal dez  : std_logic_vector(4 downto 0) := "01010";
+    signal Ov, lss   : std_logic;
+begin
+    hexa_ext <= "0" & hexa;
+    Cin_ext <= "0000" & Cin;
+
+    incrementa_hexa: somador
+    generic map (
+        size => 5
+    )
+    port map (
+        A => hexa_ext,
+        B => Cin_ext,
+        S => '0',
+        F => hexa_int,
+        Z => open,
+        N => open,
+        Ov => Ov,
+        Co => open
+    );
+
+    hexa_2 <= "0" & hexa_int (3 downto 0);
+
+    subtrai_dez: somador
+    generic map (
+        size => 5
+    )
+    port map (
+        A => hexa_2,
+        B => dez,
+        S => '1',
+        F => dec_int,
+        Z => open,
+        N => lss,
+        Ov => open,
+        Co => open
+    );
+
+    dec_ext <= hexa_2 when lss = '1' else dec_int;
+
+    Cout <= Ov or (not lss);
+
+    dec <= dec_ext(3 downto 0);
+end architecture dataflow;
---
+++ Projeto/Semana_3/somador.vhd
@@ -0,0 +1,71 @@
+--------------------------------------------------------------------
+-- Arquivo   : somador.vhd
+-- Projeto   : Projeto PCS3635 2023
+--------------------------------------------------------------------
+-- Descricao :
+--    Unidade combinatória para soma
+--------------------------------------------------------------------
+-- Revisoes  :
+--     Data        Versao  Autor             Descricao
+--     10/11/2021  1.0     Rafael Innecco    veesão utilziada em uma ULA
+--     17/03/2023  2.0     Rafael Innecco    Versão para uso no projeto, troca de fomrado bit para std_logic
+--     24/03/2023  2.1     Rafael Innecco    Adiciona subtração
+--------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity somador is
+    generic (
+        size : natural := 8
+    );
+
+    port (
+        A, B    : in std_logic_vector (size - 1 downto 0); --inputs
+        S       : in std_logic; -- Seleciona se será feita subtração
+        F       : out std_logic_vector (size - 1 downto 0);
+        Z       : out std_logic; --zero flag
+        N       : out std_logic; --negative flag
+        Ov      : out std_logic; --overflow flag
+        Co      : out std_logic --carry out flag
+    );
+end entity somador;
+
+architecture dataflow of somador is
+
+    signal SAI, B_int  : std_logic_vector (size - 1 downto 0); --Sinais intermediários
+    signal CA   : std_logic_vector (size downto 0); -- Sinais utilizados para realizar o carry na soma e na subtração
+
+begin
+    B_int <= not B when S = '1' else B;
+    -- Implementação da soma por Carry-lookahead
+    CA(0) <= '1';
+    GEN_2: for j in 1 to (size) generate
+        CA(j) <= (A(j - 1) and B(j - 1)) or ((A(j - 1) or B(j - 1)) and CA(j - 1)); --Carry da soma do j-ésimo std_logic
+    end generate GEN_2;
+
+    GEN_3: for j in 0 to (size - 1) generate
+        SAI(j) <= A(j) xor B(j) xor CA(j);
+    end generate GEN_3;
+
+    -- Implementação de flags
+
+    -- Flag de overflow
+    Ov <= CA(size) xor CA(size - 1);
+    --
+
+    -- Flag  de Carry-out
+    Co <= CA(size);
+    --
+
+    N <= SAI(size - 1);
+
+    Z <= '1' when SAI = std_logic_vector(to_unsigned(0, size)) else
+         '0';
+    --
+
+    F <= SAI;
+
+end architecture dataflow;
